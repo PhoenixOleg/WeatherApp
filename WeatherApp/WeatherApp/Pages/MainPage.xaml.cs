82993b3c72e59a22c36d0f9f89a7f9b4c906e11d
PhoenixOleg <metall_2000@mail.ru>
2024-05-12 03:59:30 +0300

    Добавьте файлы проекта.

---
+++ WeatherApp/WeatherApp/Pages/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace WeatherApp.Pages
+    {
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public async void GetWeather_Click(object sender, EventArgs e)
+        {
+            //.
+            await Navigation.PushAsync(new ShowWeather());
+        }
+    }
+}
---
+++ WeatherApp/WeatherApp/Pages/ShowWeather.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WeatherApp.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ShowWeather : ContentPage
+    {
+        public ShowWeather()
+        {
+            InitializeComponent();            
+            ShowContent(); 
+        }
+
+        private void ShowContent()
+        { 
+            var grid = (Grid)ContentGrid;
+            grid.Children.Clear();
+            
+            var insideThermalSensor = new StackLayout { BackgroundColor = Color.FromRgb(255, 201, 00), VerticalOptions = LayoutOptions.Fill, HorizontalOptions = LayoutOptions.Fill };
+            insideThermalSensor.Children.Add(new Label { Text = "Inside", FontSize = Device.GetNamedSize(NamedSize.Title, typeof(Label) ), HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.EndAndExpand });
+            insideThermalSensor.Children.Add(new Label { Text = "+26 °C", FontSize = Device.GetNamedSize(NamedSize.Header, typeof(Label)), HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.CenterAndExpand });
+            grid.Children.Add(insideThermalSensor, 0, 0);
+
+            var outsideThermalSensor = new StackLayout { BackgroundColor = Color.FromRgb(92, 205, 201), VerticalOptions = LayoutOptions.Fill, HorizontalOptions = LayoutOptions.Fill };
+            outsideThermalSensor.Children.Add(new Label { Text = "Outside", FontSize = Device.GetNamedSize(NamedSize.Title, typeof(Label)), HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.EndAndExpand });
+            outsideThermalSensor.Children.Add(new Label { Text = "-15 °C", FontSize = Device.GetNamedSize(NamedSize.Header, typeof(Label)), HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.CenterAndExpand });
+            grid.Children.Add(outsideThermalSensor, 0, 1);
+
+            var pressureSensor = new StackLayout { BackgroundColor = Color.FromRgb(00, 101, 97) };
+            pressureSensor.Children.Add(new Label { Text = "Pressure", FontSize = Device.GetNamedSize(NamedSize.Title, typeof(Label)), HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.EndAndExpand });
+            pressureSensor.Children.Add(new Label { Text = "760 mm", FontSize = Device.GetNamedSize(NamedSize.Header, typeof(Label)), HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.CenterAndExpand });
+            grid.Children.Add(pressureSensor, 0, 2);
+        }
+    }
+}
